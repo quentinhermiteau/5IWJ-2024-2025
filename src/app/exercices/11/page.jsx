4e1a9d6ca66795ad16808bedc8abbff9f4fcd94d
Quentin Hermiteau <quentinhermiteau@gmail.com>
2025-03-27 10:51:55 +0100

    correction exo 11

--- src/app/exercices/11/page.jsx
+++ src/app/exercices/11/page.jsx
@@ -1,16 +1,43 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useReducer } from "react";
 import Slider from "./Slider";
 
-export default function Counter() {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+const initialState = {
+  count: 0,
+  step: 1,
+};
 
-  const handleIncrement = () => setCount(count + step);
-  const handleDecrement = () => setCount(count - step);
-  const handleReset = () => setCount(0);
-  const handleUpdateStep = (step) => setStep(step);
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { ...state, count: state.count + state.step };
+    case "DECREMENT":
+      return { ...state, count: state.count - state.step };
+    case "RESET":
+      return initialState;
+    case "UPDATE_STEP":
+      return { ...state, step: action.step };
+    default:
+      return state;
+  }
+};
+
+export default function Counter() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleIncrement = () => dispatch({ type: "INCREMENT" });
+  const handleDecrement = () => dispatch({ type: "DECREMENT" });
+  const handleReset = () => dispatch({ type: "RESET" });
+  const handleUpdateStep = (step) => dispatch({ type: "UPDATE_STEP", step });
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      dispatch({ type: "INCREMENT" });
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <main>
@@ -18,13 +45,13 @@ export default function Counter() {
         À l'aide de useEffect et setInterval, faire en sorte que le compteur
         s'incrémente automatiquement toutes les secondes.
       </div>
-      <h1>{count}</h1>
+      <h1>{state.count}</h1>
       <div>
         <button onClick={handleDecrement}>-</button>
         <button onClick={handleIncrement}>+</button>
         <button onClick={handleReset}>0</button>
       </div>
-      <Slider min={1} max={10} onChange={handleUpdateStep} />
+      <Slider min={1} max={10} value={state.step} onChange={handleUpdateStep} />
     </main>
   );
 }
